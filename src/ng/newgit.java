35032d976f511437957ce37959e8f5164333ff00
Rohan Singh Slathia <rohan_slathia@persistent.com>
2022-01-18 15:54:03 +0530

    first try

---
+++ src/ng/newgit.java
@@ -0,0 +1,12 @@
+package ng;
+
+public class newgit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("I'm Ironman *snaps gauntlet*");
+		System.out.print("I'm Ironman *snaps gauntlet*");
+
+	}
+
+}
