4a86508768c51d6dddf1ccef726255c0eec2b900
Rohan Singh Slathia <rohan_slathia@persistent.com>
2022-01-18 16:00:27 +0530

    modified try

--- src/ng/newgit.java
+++ src/ng/newgit.java
@@ -6,6 +6,8 @@ public class newgit {
 		// TODO Auto-generated method stub
 		System.out.print("I'm Ironman *snaps gauntlet*");
 		System.out.print("I'm Ironman *snaps gauntlet*");
+		System.out.print("I'm Ironman *snaps gauntlet*");
+		System.out.print("I'm Ironman *snaps gauntlet*");
 
 	}
 
